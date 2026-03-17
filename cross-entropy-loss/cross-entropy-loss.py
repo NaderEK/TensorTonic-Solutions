f39d04a142937b48723c9c7f3a3f213a25ffe838
Nader Essam <nader.e.kassem@gmail.com>
2026-03-17 22:22:19 +0100

    Add solution for cross-entropy-loss

---
+++ cross-entropy-loss/cross-entropy-loss.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def cross_entropy_loss(y_true, y_pred):
+    """
+    Compute average cross-entropy loss for multi-class classification.
+    """
+    # Write code here
+    y_pred, y_true = np.asarray(y_pred), np.asarray(y_true)
+    if y_pred.shape[0] != y_true.shape[0]:
+        return None
+    N = y_true.shape[0]
+    correct_probs = []
+
+    for true, probs in zip(y_true, y_pred):
+        correct_probs.append(probs[true])
+    loss = - np.mean(np.log(correct_probs))
+
+    return loss
